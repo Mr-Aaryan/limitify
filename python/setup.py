0d222f7b22d5720c0399863980cdf6e9b51aca5f
Mr-Aaryan <kc.aryan3536@gmail.com>
2025-06-11 12:40:42 +0545

    mimnor fix

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name="limitify",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple api rate-limiting library with analytics support",
     long_description=open('README.md').read() if os.path.exists('README.md') else '',
     long_description_content_type='text/markdown',
